f3061ec30e05621f6c716439bb0a0825613eab27
Bret <Bretren2013@gmail.com>
2025-12-03 22:26:12 -0800

    update config

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,22 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+	adapter: adapter({
+		// See below for an explanation of these options
+		config: undefined,
+		platformProxy: {
+			configPath: undefined,
+			environment: undefined,
+			persist: undefined
+		},
+		fallback: 'plaintext',
+		routes: {
+			include: ['/*'],
+			exclude: ['<all>']
+		}
+	})
+}
 };
 
 export default config;
