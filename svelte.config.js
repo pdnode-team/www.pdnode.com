8884c3a300396d9588a74197f42e5f378043466b
Bret <Bretren2013@gmail.com>
2025-12-03 22:20:41 -0800

    update config

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,14 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+    adapter: adapter({
+      // 启用 Node.js 兼容性标志
+      platform: {
+        compatibilityFlags: ['nodejs_compat'] 
+      }
+    })
+  }
 };
 
 export default config;
