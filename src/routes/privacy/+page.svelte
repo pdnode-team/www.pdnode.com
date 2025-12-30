9fd6f9d2f43748b1d053a88b15a4c10bd2c65d48
Bret <Bretren2013@gmail.com>
2025-12-30 15:56:52 -0800

    add terms and privacy

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    window.location.href = 'https://docs.pdnode.com/docs/platform/privacy'
+  });
+</script>
+
+Redirecting for you
---
+++ src/routes/terms/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    window.location.href = 'https://docs.pdnode.com/docs/platform/terms'
+  });
+</script>
+
+Redirecting for you
