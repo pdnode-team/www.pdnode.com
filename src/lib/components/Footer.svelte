f19c729156f5c12fb7314d76a135fd7524653ec0
BretRen <bretren2013@gmail.com>
2026-01-05 13:59:57 -0800

    Add Terms and Privacy Link in Footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import * as m from "$lib/paraglide/messages"
+  import { resolve } from '$app/paths';
 </script>
 
-<footer class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
-  <aside>
+<footer class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4 flex items-center justify-center w-full">
+  <aside class="flex items-center justify-center flex-row">
     <p>{ m.footer_copyright()} © {new Date().getFullYear()} - {m["footer_copyright_all-right-reserved-by"]()}</p>
+    <a class="link" target="_blank" href={resolve("/terms")}>Terms of Service</a>
+    <a class="link" target="_blank" href={resolve("/privacy")}>Privacy Policy</a>
   </aside>
 </footer>
--- src/routes/terms/+page.svelte
+++ src/routes/terms/+page.svelte
@@ -6,4 +6,6 @@
   });
 </script>
 
-Redirecting for you
+<div class="flex justify-center item-center my-4">
+  Redirecting for you
+</div>
